95c0f8e5b32b06e4e321df1d72b5e97147ba067a
Manolete <manuelcjt@hotmail.com>
2023-03-28 16:04:26 -0500

    MontamosDetalleProducto

--- Rocosa/Controllers/HomeController.cs
+++ Rocosa/Controllers/HomeController.cs
@@ -29,6 +29,20 @@ namespace Rocosa.Controllers
             return View(HomeVM);
         }
 
+        [HttpPost]
+        public IActionResult Detalle(int Id)
+        {
+            DetalleVM detalleVM = new DetalleVM()
+            {
+                Producto = _db.Producto.Include(c => c.Categoria).Include(t => t.TipoAplicacion)
+                  .Where(p => p.Id == Id).FirstOrDefault(),
+
+                ExisteEnCarro = false
+
+            };
+            return View(detalleVM);
+        }
+
         public IActionResult Privacy()
         {
             return View();
---
+++ Rocosa/Models/ViewModels/DetalleVM.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Rocosa.Models.ViewModels
+{
+    public class DetalleVM
+    {
+
+        //Inicializamos aqui para no hacerlo en el controlador  
+        public DetalleVM()
+        {
+            Producto= new Producto();
+
+        }
+
+        public Producto Producto  { get; set; }
+        public bool ExisteEnCarro { get; set; }
+    }
+}
